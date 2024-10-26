1f35bc62c110af6c4189a5ad338ac4592d01bf81
Dhruvin <therockstar3007@gmail.com>
2024-10-26 16:53:37 +0530

    any3

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -19,7 +19,8 @@ export default function LoginPage() {
             const response = await axios.post("/api/users/login", user);
             console.log("Login successful:", response.data);
             router.push("/profile");
-        } catch (error: any) {
+        } // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        catch (error: any) {
             console.log("Login failed", error.message);
         } finally {
             setLoading(false);
